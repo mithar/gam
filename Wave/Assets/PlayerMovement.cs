a38548ce7344933e1cc699ed26f83d349033368a
Dennis <castrodennis97@gmail.com>
2018-01-26 21:29:16 -0500

    movement
    Not done so far

---
+++ Wave/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 50f;
+	public bool grounded; // might not even need this
+	private Rigidbody2D lifeGuard;
+
+	// Use this for initialization
+	void Start () {
+		lifeGuard = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis ("Horizontal");
+	//	lifeGuard.AddForce ((Vector2.right * speed) * h);
+
+		if (Input.GetKey (KeyCode.D)) {
+
+			lifeGuard.AddForce ((Vector2.right * speed) * h);
+		}
+
+	}
+
+
+}
